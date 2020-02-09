bdd8e82cfce04fc6b7ded1ceb8fe7e6b5f6fd6e1
Aditya Gupta <adityaguptagkp30@gmail.com>
2020-02-10 00:35:09 +0530

    final complain box with complete design

--- test.php
+++ test.php
@@ -1,30 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<body>
-    <form action="test.php" method="post" enctype="multipart/form-data">
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
 
-        complainant:<select name="complainant">
-            <option value="none">none</option>
-            <option value="faculty">faculty</option>
-            <option value="staff">staff</option>
-            <option value="student">student</option>
-            <option value="employer">employer</option>
-            <option value="alumini">alumni</option>
-            <option value="parents">parents</option>
-        </select><br /><br />
-
-        Name: <input type="text" name="yname"><br /><br />
-        Mobile:<input type="text" name="mobile"><br /><br />
-        Email:<input type="email" name="email"><br /><br />
-        Address:<input type="text" name="address"><br /><br />
-        Complaint:<input type="text" name="complaint"><br /><br />
-
-        Select image to upload:
-        <input type="file" name="image" />
-        </br>
-        <input type="submit" name="submit" value="SUBMIT" />
-    </form>
+<body class="bg-success p-4">
+    <div class="container w-50 p-3 bg-white d-flex align-items-center" style="border: 2px solid white; border-radius: 50px 20px;">
+    <h1>AKGEC GRIEVANCE</h1>
+        <form class="form-group" action="test.php" method="post" enctype="multipart/form-data">
+        <label for="complainant">Complainant: </label>
+            <select class="form-control col-12" name="complainant" required>
+                <option value="none" disabled selected>none</option>
+                <option value="faculty">faculty</option>
+                <option value="staff">staff</option>
+                <option value="student">student</option>
+                <option value="employer">employer</option>
+                <option value="alumini">alumni</option>
+                <option value="parents">parents</option>
+            </select>
+            <label for="yname">Name: </label>
+            <input class="form-control col-12" type="text" name="yname" autocomplete="off" required>
+            <label for="mobile">Mobile: </label>
+            <input class="form-control col-12" type="number" name="mobile" autocomplete="off" required>
+            <label for="email">Email: </label>
+            <input class="form-control col-12" type="email" name="email" autocomplete="off" required>
+            <label for="address">Address: </label>
+            <input class="form-control col-12" type="text" name="address" autocomplete="off" required>
+            <label for="complaint">Complaint: </label>
+            <input class="form-control col-12" type="text" name="complaint" autocomplete="off" required>
+            <label for="image">Select image to upload: </label>
+            <input class="form-control col-12 mb-2" type="file" name="image" required/>
+            <input class="btn btn-primary" type="submit" name="submit" value="SUBMIT" />
+        </form>
+    </div>
 </body>
 
 </html>
@@ -59,57 +69,54 @@ if(isset($_POST["submit"])){
         //Insert image content into database
         $insert = $db->query("INSERT into images (image, created,complainant,name,mobile,email,address,complaint) VALUES ('$imgContent', '$dataTime','$complainant','$name','$mobile','$email','$address','$complaint')");
         if($insert){
-            echo "File uploaded successfully.";
+            echo "<script> alert('File uploaded successfully.');</script>";
         } else {
-            echo "File upload failed, please try again.";
+            echo "<script> alert('File upload failed, please try again.');</script>";
         }   
      }else echo "Please select an image file to upload.";
+                            
+     require './phpm/helo.php';
+     // $email1='adityaguptagkp08@gmail.com';
+     $mail = new PHPMailer;
+     //$mail->SMTPDebug = 4;                               // Enable verbose debug output
+     $mail->isSMTP();                                      // Set mailer to use SMTP
+     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+     $mail->SMTPAuth = true;                               // Enable SMTP authentication
+     $mail->Username = 'adityaguptagkp30@gmail.com';                 // SMTP username
+     $mail->Password = 'Tt2338627*';                           // SMTP password
+     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+     $mail->Port = 587;                                    // TCP port to connect to
+     $mail->setFrom('adityaguptagkp30@gmail.com', 'AKGEC GRIEVANCE');
+     $mail->addAddress($email);     // Add a recipient              // Name is optional
+     $mail->addAddress('indiaetoos1@gmail.com');  
+     $mail->addAddress('indiaetoos1@gmail.com');  
+     //$mail->addReplyTo(EMAIL);
+     
+     $mail->isHTML(true);                                  // Set email format to HTML
+     
+     $mail->Subject = 'AKGEC GRIEVANCE GUIDE WELCOMES';
+     
+     $mail->Body    = 'Your response has been submitted sucessfully.';
+     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+     
+     if(!$mail->send()) {
+         echo "<script> alert('Message could not be sent.');</script>";
+         echo 'Mailer Error: ' . $mail->ErrorInfo;
+     } else {
+         echo 'Message has been sent to your email:';
+         echo $email;
+     }
+     
+     
+                         $correct_response = 'Registration successfull...';
+                         // header("refresh:3;url=tlogin.php");
+     
+                         // to take to login page within 2 sec
+                      //                
+                   
+                     // else
+                     // {
+                     //     $response = 'Something went wrong';
+                     // }     
 }
-                             
-require './phpm/helo.php';
-// $email1='adityaguptagkp08@gmail.com';
-$mail = new PHPMailer;
-//$mail->SMTPDebug = 4;                               // Enable verbose debug output
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'adityaguptagkp30@gmail.com';                 // SMTP username
-$mail->Password = '****';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
-$mail->setFrom('adityaguptagkp30@gmail.com', 'AKGEC GRIEVANCE');
-$mail->addAddress($email);     // Add a recipient              // Name is optional
-$mail->addAddress('indiaetoos1@gmail.com');  
-$mail->addAddress('indiaetoos1@gmail.com');  
-//$mail->addReplyTo(EMAIL);
-
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'AKGEC GRIEVANCE GUIDE WELCOMES';
-
-$mail->Body    = 'Your response has been submitted sucessfully.';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message has been sent please verify your email:';
-    echo $email;
-}
-
-
-                    $correct_response = 'Registration successfull...';
-                    // header("refresh:3;url=tlogin.php");
-
-                    // to take to login page within 2 sec
-                 //                
-              
-                // else
-                // {
-                //     $response = 'Something went wrong';
-                // }
-            
-                
-    
 ?>
